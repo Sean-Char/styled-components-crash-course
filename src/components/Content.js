ab4272ceadd88903c3daaa929b8d837392a8b355
Sean Charoutanh <scharoutanh@gmail.com>
2021-09-13 12:23:08 -0600

    initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Content = () => {
+    return (
+        <div className='section'>
+            <h1>Section</h1>
+        </div>
+    )
+}
+
+export default Content
